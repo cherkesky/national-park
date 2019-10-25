c11dd2255fcec90166fac43fa90dd35038cc0368
Cherkesky <guy@cherkesky.com>
2019-10-25 13:55:59 -0500

    json iteration and basic DOM printing completed

--- src/main.js
+++ src/main.js
@@ -12,9 +12,20 @@ for (i=0; i<jsonfiedResponse.length; i++){
 console.log (jsonfiedResponse[i].name)
 console.log (jsonfiedResponse[i].state)
 console.log (jsonfiedResponse[i].visited)
+
+// document.getElementById("#results-container").innerHTML = (jsonfiedResponse[i].name)
+// document.getElementById("#results-container").innerHTML = (jsonfiedResponse[i].state)
+// document.getElementById("#results-container").innerHTML = (jsonfiedResponse[i].visited)
+
+document.querySelector("#results-container").innerHTML += `<h3>Park:</h3> ${jsonfiedResponse[i].name}` 
+document.querySelector("#results-container").innerHTML += `<h3>Sate:</h3> ${jsonfiedResponse[i].state}` 
+document.querySelector("#results-container").innerHTML += `<h3>Visted:</h3> ${jsonfiedResponse[i].visited}` 
+
+
+
+
 }
 // insert code here for json iteration
-
 }
 
 
