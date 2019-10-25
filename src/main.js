4465b0f919d4a6fafaae3bf9b9fabd0bd7f18b7c
Cherkesky <guy@cherkesky.com>
2019-10-25 15:57:53 -0500

    successful calls to the Dark Sky API. Printing all the info to the console at this point

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 console.log("national parks js file")
+
+
 const resultsContainer= document.querySelector("#results-container");
 
-fetch ("http://localhost:8088/parks") // fetching the local json server
+// ***************************************************************************
+// Initial fetch call to the local json server
+// ***************************************************************************
+fetch ("http://localhost:8088/parks") 
 .then(response => response.json())
 .then(jsonfiedResponse=> jsonIterator(jsonfiedResponse))
 
@@ -9,19 +14,29 @@ jsonIterator=(jsonfiedResponse)=>{
  console.log(jsonfiedResponse)
 
 for (i=0; i<jsonfiedResponse.length; i++){
-let parkName = jsonfiedResponse[i].name
-let parkState = jsonfiedResponse[i].state
-let parkVisted = jsonfiedResponse[i].visited
+    let parkName = jsonfiedResponse[i].name
+    let parkState = jsonfiedResponse[i].state
+    let parkVisted = jsonfiedResponse[i].visited
+    let parkLong = jsonfiedResponse[i].longitude
+    let parkLat = jsonfiedResponse[i].latitude
 
-domPrinter(parkName, parkState, parkVisted); // calling the dom printer function
+domPrinter(parkName, parkState, parkVisted, parkLong, parkLat); // calling the dom printer function
 
 console.log (parkName)
 console.log (parkState)
 console.log (parkVisted)
+console.log (parkLong)
+console.log (parkLat)
+
+
 }
 }
 
-domPrinter=(parkName,ParkState, parkVisted)=>{
+// ***************************************************************************
+// The DOM printer helper function
+// ***************************************************************************
+
+domPrinter=(parkName,ParkState, parkVisted, parkLong, parkLat)=>{
   // If the park has been visited, the article tag should have a red dashed border. If the park has not been visited, it should have a green solid border.
   const parkArticleEl = document.createElement("article")
   const parkNameEl = document.createElement("h3")
@@ -37,13 +52,25 @@ domPrinter=(parkName,ParkState, parkVisted)=>{
   }else{
     parkArticleEl.classList.add("not-visited")
   }
-
   resultsContainer.appendChild(parkArticleEl)
+  fetchWeather(parkLong, parkLat); // calling the Dark Sky API
+
 
 }
+// ***************************************************************************
+// Calling the Dark Sky API to get the weather info based on long/lat coordinates
+// ***************************************************************************
+ fetchWeather=(parkLong, parkLat)=>{
+  fetch (`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${apiKey}/${parkLat},${parkLong}`)
+  .then(darkSkyresponse=>darkSkyresponse.json())
+  .then(darkSkyJsonfiedResponse=>weatherInfoGrabber(darkSkyJsonfiedResponse))
+}
 
+// ***************************************************************************
+// Grabbing the relevant info from the Dark Sky API json response
+// ***************************************************************************
+weatherInfoGrabber=(darkSkyJsonfiedResponse)=> {
+console.log(darkSkyJsonfiedResponse)
+}
 
-fetch (`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${apiKey}/37.8267,-122.4233`)
-.then(darkSkyresponse=>darkSkyresponse.json())
-.then(darkSkyJsonfiedResponse=>console.log(darkSkyJsonfiedResponse))
 
