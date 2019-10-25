f2be6be6880fc5d0315e4bb2cd0e68d6f7bcb6ac
Cherkesky <guy@cherkesky.com>
2019-10-25 13:44:24 -0500

    iterating through the json response

--- src/main.js
+++ src/main.js
@@ -2,4 +2,32 @@ console.log("national parks js file")
 
 fetch ("http://localhost:8088/parks")
 .then(response => response.json())
-.then(jsonfiedResponse=> console.log(jsonfiedResponse))
+.then(jsonfiedResponse=> jsonIterator(jsonfiedResponse))
+
+
+jsonIterator=(jsonfiedResponse)=>{
+console.log(jsonfiedResponse)
+
+for (i=0; i<jsonfiedResponse.length; i++){
+console.log (jsonfiedResponse[i].name)
+console.log (jsonfiedResponse[i].state)
+console.log (jsonfiedResponse[i].visited)
+}
+// insert code here for json iteration
+
+}
+
+
+
+
+
+
+
+domPrinter=()=>{
+  //insert code here for buiding the DOM elements
+    // `<article>
+    //     <h3>Park Name</h3>
+    //     <p>State the park in located in</p>
+    // </article>`
+
+}
